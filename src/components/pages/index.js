77f25281f74eaa773fad2d38f3087fca91e971c3
muchtaromyahya <muchtaromyahya@student.telkomuniversity.ac.id>
2020-11-07 10:30:47 +0700

    add Info Corona

--- src/components/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home';
-export { default as Education } from './Education';
-export { default as Skill } from './Skill';
-export { default as Experience } from './Experience';
-export { default as ContactUs } from './ContactUs';
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,29 +1,41 @@
-import { Home, Education, Skill, Experience, ContactUs} from '../components/pages';
+import { Home, Profile, Contact, Login, ProfileId, InfoCorona } from '../pages';
 
 const routes = [
-    {
-        path : '/contact',
-        component : ContactUs,
-    },
-    {
-        path : '/experience',
-        component : Experience,
-    },
-    {
-        path : '/skill',
-        component : Skill,
-    },
-    {
-        path : '/education',
-        component : Education,
-    },
-    {
-        path : '/home',
-        component : Home,
-    },
-    {
-        path : '',
-        component : Home,
-    },
-]
+  {
+    path: '/profile/:profileId',
+    component: ProfileId,
+    isPublic: false,
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    isPublic: false,
+  },
+  {
+    path: '/contact',
+    component: Contact,
+    isPublic: true,
+  },
+  {
+    path: '/home',
+    component: Home,
+    isPublic: true,
+  },
+  {
+    path: '/login',
+    component: Login,
+    isPublic: true,
+  },
+  {
+    path: '/infoCorona',
+    component: InfoCorona,
+    isPublic: true,
+  },
+  {
+    path: '/',
+    component: Home,
+    isPublic: true,
+  },
+];
+
 export default routes;
